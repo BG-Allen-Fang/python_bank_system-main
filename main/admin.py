3b289e8a2101fc7e67973fdde593fbb29e3590bb
alenman47 <alenman47@gmail.com>
2021-04-23 07:39:19 +0600

    Initial commit

---
+++ main/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from .models import *
+
+admin.site.register(Customer)
+admin.site.register(Service_type)
+admin.site.register(Account)
+admin.site.register(Service)
+admin.site.register(Account_type)
+admin.site.register(Transactions_history)
